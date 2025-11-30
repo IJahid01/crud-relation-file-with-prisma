4225b4ca01f3b3316c6a647082457442acf2b3e7
JAHID EKBAL MALLICK <jahidekbalmallick@gmail.com>
2025-11-30 13:20:18 +0530

    App name change, teacher and student  page metadata and linking

--- src/app/student/create/page.tsx
+++ src/app/student/create/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Create Student | User CRUD App",
+	description: "Create student page of User CRUD App",
+};
+
 const page = () => {
 	return <></>;
 };
--- src/app/teacher/create/page.tsx
+++ src/app/teacher/create/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Create teacher | User CRUD App",
+	description: "Create teacher page of User CRUD App",
+};
+
 const page = () => {
 	return <></>;
 };
